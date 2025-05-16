ac7249a5786abd39dc37a3098f5a48c478cfde53
LucaoCorrea <lucascorrea1505@gmail.com>
2025-05-16 14:56:05 -0300

    .

--- quickNote/src/main/java/com/quickNote/quickNote/QuickNoteApp.java
+++ quickNote/src/main/java/com/quickNote/quickNote/QuickNoteApp.java
@@ -6,18 +6,21 @@ import javafx.scene.Scene;
 import com.quickNote.quickNote.controller.QuickNoteController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class QuickNoteApp extends Application {
 
+    private static ApplicationContext springContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(QuickNoteApp.class, args);
+        springContext = SpringApplication.run(QuickNoteApp.class, args);
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        QuickNoteController controller = new QuickNoteController();
+        QuickNoteController controller = springContext.getBean(QuickNoteController.class);
         Scene scene = new Scene(controller.createLayout(), 600, 400);
 
         primaryStage.setTitle("QuickNote");
